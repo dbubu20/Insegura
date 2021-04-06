39bb13fe4d2bdc685e5b81b2387908157c00f6b0
dbubu20 <82052030+dbubu20@users.noreply.github.com>
2021-04-06 15:39:45 -0600

    Delete Insegura directory

--- Insegura/LOGGIN/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$conexion=mysqli_connect("localhost","root","123","test");
-
-
-?>
-<H1>CONECTADO</H1>
--- Insegura/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$conexion=mysqli_connect("localhost","root","123","test");
-  if ($conexion->connect_error) {
-        echo "No sea podido conectar la base de datos" .$conn->connect_error;
-  }
- ?>
